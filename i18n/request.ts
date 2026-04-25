e91341439e464370564928d2a7895dc11e48748b
Brendon Franco <contact@brends.me>
2026-04-25 09:07:21 -0300

    feat: merge prayer and UI i18n namespaces in request config

--- i18n/request.ts
+++ i18n/request.ts
@@ -15,15 +15,39 @@ const messagesLoaders: Record<SupportedLocale, () => Promise<MessagesModule>> =
     la: () => import("./ui/la.json") as Promise<MessagesModule>,
   };
 
+const prayerMessagesLoaders: Record<
+  SupportedLocale,
+  () => Promise<MessagesModule>
+> = {
+  en: () => import("./prayers/en.json") as Promise<MessagesModule>,
+  "pt-br": () => import("./prayers/pt-br.json") as Promise<MessagesModule>,
+  la: () => import("./prayers/la.json") as Promise<MessagesModule>,
+};
+
 export default getRequestConfig(async () => {
   const cookieStore = await cookies();
 
   const locale = parseSettingsCookie(
     cookieStore.get(SETTINGS_COOKIE_NAME)?.value
   ).uiLanguage;
+  const [uiMessages, prayerMessages] = await Promise.all([
+    messagesLoaders[locale](),
+    prayerMessagesLoaders[locale](),
+  ]);
+
+  const uiPrayerMessages =
+    typeof uiMessages.default.prayer === "object" && uiMessages.default.prayer
+      ? uiMessages.default.prayer
+      : {};
 
   return {
     locale,
-    messages: (await messagesLoaders[locale]()).default,
+    messages: {
+      ...uiMessages.default,
+      prayer: {
+        ...uiPrayerMessages,
+        ...prayerMessages.default,
+      },
+    },
   };
 });
